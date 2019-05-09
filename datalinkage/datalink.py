49fb1b2e7127eb4bcdc131dd009cb3da595417c8
Laura Zheng <laurazheng@wireless-10-105-53-199.umd.edu>
2019-05-09 12:14:00 -0400

    integrated parser with main python file

--- datalinkage/datalink.py
+++ datalinkage/datalink.py
@@ -12,7 +12,7 @@ avg_wage = 13.72
 
 # file names
 bob_name = 'mastersales.xlsx'
-humanity_name = 'humanity_eventid.xlsx'
+humanity_name = '2019-03 Humanity'
 sel_name = '2019-03 SEL.xlsx'
 
 # output files
@@ -86,10 +86,24 @@ def threefour(row):
 	else:
 		return 0
 
-def combine(): 
+def parse_eventid():
+	data = pd.read_excel("input/" + humanity_name + ".xlsx", sheet_name=0)
+	data.dropna(subset=['shift_title'], inplace = True)
+
+	new = data["shift_title"].str.split("~", n = 1, expand = True)
+	data.drop(columns =["shift_title"], inplace = True)
+	#print(new)
+	data["shift_title"] = new[0] 
+	data["event_id"] = new[1]
+	data = data[['eid','location','start_day','start_time','end_time','total_time','shift_title','event_id']]
+
+	return data
+	#data.to_excel("input/" + file_out + ".xlsx", index=None)
+
+def combine(humanity): 
 	# load data files
 	bob = pd.read_excel('input/' + bob_name, sheet_name=0)
-	humanity = pd.read_excel('input/' + humanity_name)
+	#humanity = pd.read_excel('input/' + humanity_name)
 	sel = pd.read_excel('input/' + sel_name, sheet_name=0)
 
 	# no need to filter hellofresh from bob anymore
@@ -204,4 +218,5 @@ def combine():
 	combined.to_excel('output/' + 'combined.xlsx', index=None)
 
 if __name__ == "__main__":
-	combine()
+	humanity = parse_eventid()
+	combine(humanity)
